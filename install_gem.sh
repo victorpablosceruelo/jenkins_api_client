95056064573d5d6dbab0f8e44a0a547205a54d67
Your Name <you@example.com>
2020-04-14 15:50:20 +0200

    Improved installation process ...

--- install_gem.sh
+++ install_gem.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+find /usr/local/ | grep -i jenkins_api_client
+
+find /var/www/redmine-4.0/ | grep -v /gems_from_source/ | grep -i jenkins_api_client
+
+read -p "Press ctrl+c to abort if you did not remove the previous files ... " yn
+
 http_proxy=http://artefactos-ic-g.scae.redsara.es:3128 https_proxy=http://artefactos-ic-g.scae.redsara.es:3128 bundle install
 
 gem build jenkins_api_client
 
 echo "Esto tarda pq intenta conectar con el repositorio rubygems y no le hemos puesto proxy (innecesario)." 
-gem install jenkins_api_client-1.5.3.gem
+http_proxy=http://artefactos-ic-g.scae.redsara.es:3128 https_proxy=http://artefactos-ic-g.scae.redsara.es:3128 gem install jenkins_api_client-1.5.3.gem
 
 
